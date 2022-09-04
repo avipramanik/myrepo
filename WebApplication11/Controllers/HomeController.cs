90b46217a005ca97a055e57c490ec89588312377
avipramanik <109804093+avipramanik@users.noreply.github.com>
2022-09-04 18:19:07 +0530

    s
    test

---
+++ WebApplication11/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult EmpDetails()
+        {
+            //Creating List    
+            List<Employee> ObjEmp = new List<Employee>()
+        {    
+            //Adding records to list    
+            new Employee {Id=1,Name="Vithal Wadje",City="Latur",Address="Kabansangvi" },
+            new Employee {Id=2,Name="Sudhir Wadje",City="Mumbai",Address="Kurla" }
+            };
+            //return list as Json    
+            return Json(ObjEmp, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
